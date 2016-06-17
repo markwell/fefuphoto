4bff7cbbea4c97cdf8cdc6cbce7cf1acdd9a7d2a
Mark <mrak.mk@ya.ru>
2016-06-17 23:18:08 +1000

    Некоторые исправления по верстке
    :sailboat:

--- client/pages/member/member.js
+++ client/pages/member/member.js
@@ -3,7 +3,10 @@ Template.member.helpers({
     return Team.findOne(this.id);
   },
   reports: function() {
-    return Reports.find({'photographer':this.id});
+    return Reports.find({'photographer':this.id},{sort: {date: -1}});
+  },
+  reportDate: function(date) {
+    return moment(date).locale('ru').format('MMMM DD, dd');
   },
   photo: function(id) { return CrudFiles.findOne(id); },
   getNets: function(nets) {
